3b1f2579926631cb175cc5666a374b9266336ef6
APN-Pucky <APN-Pucky@users.noreply.github.com>
2017-11-04 16:08:44 +0100

    Finaly!

--- build/windows/build.sh
+++ build/windows/build.sh
@@ -9,13 +9,13 @@ MXE_TARGET=i686-w64-mingw32.static
 ${MXE_DIR}/usr/bin/${MXE_TARGET}-cmake . -Bbuild-dir -DVERSION:STRING="${TRAVIS_TAG}"
 cmake --build build-dir 
 
-mv build-dir/tuo.exe ../$TUO_FILE_32
+mv build-dir/tuo.exe $TUO_FILE_32
 
 MXE_TARGET=x86_64-w64-mingw32.static
 
 ${MXE_DIR}/usr/bin/${MXE_TARGET}-cmake . -Bbuild-dir -DVERSION:STRING="${TRAVIS_TAG}"
 cmake --build build-dir 
 
-mv build-dir/tuo.exe ../$TUO_FILE_64
+mv build-dir/tuo.exe $TUO_FILE_64
 
 set +xue
