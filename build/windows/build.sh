5d8b200c77193751b5d3dbea3c22679362b64336
APN-Pucky <APN-Pucky@users.noreply.github.com>
2017-11-14 22:17:26 +0100

    Split Build x64 and x32

--- build/windows/build.sh
+++ build/windows/build.sh
@@ -9,17 +9,17 @@ MXE_DIR=/usr/lib/mxe
 
 MXE_TARGET=x86_64-w64-mingw32.static
 
-${MXE_DIR}/usr/bin/${MXE_TARGET}-cmake . -Bbuild-dir -DVERSION:STRING="${TRAVIS_TAG}"
-cmake --build build-dir 
+${MXE_DIR}/usr/bin/${MXE_TARGET}-cmake . -Bbuild-dir64 -DVERSION:STRING="${TRAVIS_TAG}"
+cmake --build build-dir64 
 
-mv build-dir/tuo.exe $TUO_FILE_64
+mv build-dir64/tuo.exe $TUO_FILE_64
 
 MXE_TARGET=i686-w64-mingw32.static
 
-${MXE_DIR}/usr/bin/${MXE_TARGET}-cmake . -Bbuild-dir -DVERSION:STRING="${TRAVIS_TAG}"
-cmake --build build-dir 
+${MXE_DIR}/usr/bin/${MXE_TARGET}-cmake . -Bbuild-dir32 -DVERSION:STRING="${TRAVIS_TAG}"
+cmake --build build-dir32 
 
-mv build-dir/tuo.exe $TUO_FILE_32
+mv build-dir32/tuo.exe $TUO_FILE_32
 
 
 
